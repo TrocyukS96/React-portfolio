a43e200f8b73c4a1b2c296895b7276b0ee0150af
TrocyukS96 <trotzuk.stanislav@gmail.com>
2022-10-31 13:30:42 +0300

    added changes

--- src/features/projects/Projects.tsx
+++ src/features/projects/Projects.tsx
@@ -2,23 +2,22 @@ import React from "react";
 import s from "./Projects.module.scss";
 import {Title} from "../../components/title/Title";
 import {ProjectCard} from "./projectCard/ProjectCard";
-import aeroUnionImg from './../../assets/images/myProjects/auroUnionProj.png';
-import planYourLifeImg from './../../assets/images/myProjects/planYourLifeProj.png';
-import smoothyProjImg from './../../assets/images/myProjects/organicFoodsImg.png';
-import thriveProjImg from './../../assets/images/myProjects/thriveProgImg.png';
 import newProvidenceProjImg from './../../assets/images/myProjects/newProvidenceProjImg.png';
 import archMoveProjImg from './../../assets/images/myProjects/archMoveProjImg.png';
 import driveMotoProjImg from './../../assets/images/myProjects/driveMotoProjImg.png';
-import nedvexImg from './../../assets/images/myProjects/nedvex.png';
 import learnCards from './../../assets/images/myProjects/learnCards.png';
 import socialMedia from './../../assets/images/myProjects/socialMedia.png';
+import banticom from './../../assets/images/myProjects/banticom.png';
+import crmPlatfrom from './../../assets/images/myProjects/crm.png';
 
 
-    let projectImages = [
-        {img:nedvexImg, link:'http://212.41.17.20:8080/main',phoneTitle:'Internet shop'},
-        {img:learnCards, link:'https://trocyuks96.github.io/learnCards-app/#/profile',phoneTitle:'Learn cards project'},
-        {img:socialMedia, link:'https://trocyuks96.github.io/socialMedia/#/profile',phoneTitle:'Social media project'},
-        {img:driveMotoProjImg, link:'https://trocyuks96.github.io/react-project/',phoneTitle: 'Drive moto learning project'},
+let projectImages = [
+        // {img:nedvexImg, link:'http://212.41.17.20:8080/main',phoneTitle:'Internet shop'},
+        {img:banticom, link:'https://online.prosells.ru/personal/dashboards/main',phoneTitle:'Smart admin panel'},
+        {img:crmPlatfrom, link:'http://ortoped-crm.fastweb-tech.ru/',phoneTitle:'CRM platform'},
+    {img:socialMedia, link:'https://trocyuks96.github.io/socialMedia/#/profile',phoneTitle:'Social media project'},
+    {img:learnCards, link:'https://trocyuks96.github.io/learnCards-app/#/profile',phoneTitle:'Learn cards project'},
+        // {img:driveMotoProjImg, link:'https://trocyuks96.github.io/react-project/',phoneTitle: 'Drive moto learning project'},
         {img:archMoveProjImg, link:'https://trocyuks96.github.io/arch-project/',phoneTitle: 'ArchMove learning project'},
         {img:newProvidenceProjImg, link:'https://trocyuks96.github.io/NewProvidence/',phoneTitle: 'New Providence learning project'},
         // {img:thriveProjImg, link:'https://trocyuks96.github.io/ThriveTalks-project/'},
