b7e6bad09b9b1ca54f4e3452fc5be269f9dc1d16
TrocyukS96 <trotzuk.stanislav@gmail.com>
2023-02-10 10:46:28 +0300

    fixed error

--- src/features/dal/contactsAPI.ts
+++ src/features/dal/contactsAPI.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { SMTPClient } from 'emailjs';
+import emailjs from '@emailjs/browser';
 import {RequestData} from "../../../types";
 
 
@@ -10,28 +10,42 @@ export const instance = axios.create({
     // baseURL: 'http://localhost:7542/2.0/',
 })
 
-const client = new SMTPClient({
-    user: 'Stanislav',
-    password: 'Gr$q7+kFGPc+-Gj',
-    host: 'smtp.trotzuk.stanislav@gmail.com',
-    ssl: true,
-});
+// const client = new SMTPClient({
+//     user: 'Stanislav',
+//     password: 'Gr$q7+kFGPc+-Gj',
+//     host: 'smtp.trotzuk.stanislav@gmail.com',
+//     ssl: true,
+// });
+
+// emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+//     .then((result) => {
+//         console.log(result.text);
+//     }, (error) => {
+//         console.log(error.text);
+//     });
+// };
+
 
 export const contactsApi = {
    async sendFeedBack(contactsData:RequestData){
-        try {
-            const message = await client.sendAsync({
-                text:contactsData.message,
-                from: 'you <username@trotzuk.stanislav@gmail.com>',
-                to: 'someone <someone@trotzuk.stanislav@gmail.com>, another <another@trotzuk.stanislav@gmail.com>',
-                cc: 'else <else@trotzuk.stanislav@gmail.com>',
-                subject: contactsData.subject,
-            });
-            console.log(message);
-        } catch (err) {
-            console.error(err);
-        }
+        // try {
+        //     const message = await client.sendAsync({
+        //         text:contactsData.message,
+        //         from: 'you <username@trotzuk.stanislav@gmail.com>',
+        //         to: 'someone <someone@trotzuk.stanislav@gmail.com>, another <another@trotzuk.stanislav@gmail.com>',
+        //         cc: 'else <else@trotzuk.stanislav@gmail.com>',
+        //         subject: contactsData.subject,
+        //     });
+        //     console.log(message);
+        // } catch (err) {
+        //     console.error(err);
+        // }
 
+       try {
+        const result = await emailjs.sendForm('service_po6nw7s', 'template_bpws7fa', 'asd', 'YOUR_PUBLIC_KEY')
+       }catch (e) {
+           console.log(e)
+       }
     }
 }
 
